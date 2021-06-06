fc9e041217ec37259a0ad2ba75abb0d770e7f35a
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2021-06-06 15:17:44 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SeederComponent } from './component/layout/seeder/seeder.component';
+import { PersonaComponent } from './component/persona/persona.component';
+import { PermisosComponent } from './pages/permisos/permisos.component';
+import { RolesComponent } from './pages/roles/roles.component';
+
+const routes: Routes = [{
+  path: 'persona',
+  component: PersonaComponent, 
+},{
+  path: 'roles',
+  component: RolesComponent, 
+},
+{
+  path: 'permisos',
+  component: PermisosComponent, 
+},
+{
+  path: 'seeder',
+  component: SeederComponent, 
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';//modulo para trabajar el enlase de datos con renderizado instantaneo
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,9 @@ import { PersonaComponent } from './component/persona/persona.component';
 import { NavbarComponent } from './component/layout/navbar/navbar.component';
 import { FooterComponent } from './component/layout/footer/footer.component';
 import { SeederComponent } from './component/layout/seeder/seeder.component';
+import { HeaderComponent } from './component/layout/header/header.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { PermisosComponent } from './pages/permisos/permisos.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,15 @@ import { SeederComponent } from './component/layout/seeder/seeder.component';
     PersonaComponent,
     NavbarComponent,
     FooterComponent,
-    SeederComponent
+    SeederComponent,
+    HeaderComponent,
+    RolesComponent,
+    PermisosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule//modulo para trabajar el enlace de datos
   ],
   providers: [],
   bootstrap: [AppComponent]
